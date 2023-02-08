c324bfda077d43236707b32fbeb0a15c80692077
Vivek Sharma <32999354+viveksharma8130@users.noreply.github.com>
2023-02-08 12:40:37 +0530

    final update

--- assets/index-ee6915e3.js
+++
@@ -1,1 +0,0 @@
-import{a as r,j as o}from"./index-c0278c17.js";import{C as s,R as e,a as i}from"./Row-43467c1c.js";const n=a=>r("div",{className:"footer",children:r(s,{children:r(e,{children:r(i,{lg:"12",children:r("p",{children:"Copyright © 2023 The Original Knit."})})})})}),c=({children:a})=>o("div",{className:"wrapper",children:[a,r(n,{})]});export{c as W};
