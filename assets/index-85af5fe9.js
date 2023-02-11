ddf2b11ff93fb02824e382320dcd26ad95770889
Vivek Sharma <32999354+viveksharma8130@users.noreply.github.com>
2023-02-11 20:54:38 +0530

    final

---
+++ assets/index-85af5fe9.js
@@ -0,0 +1,1 @@
+import{a as r,j as o}from"./index-1641a800.js";import{C as s,R as e,a as i}from"./Row-6c32ae88.js";const n=a=>r("div",{className:"footer",children:r(s,{children:r(e,{children:r(i,{lg:"12",children:r("p",{children:"Copyright © 2023 The Original Knit."})})})})}),c=({children:a})=>o("div",{className:"wrapper",children:[a,r(n,{})]});export{c as W};
--- assets/index-902b1dc9.js
+++
@@ -1,1 +0,0 @@
-import{a as r,j as o}from"./index-d9d1e386.js";import{C as s,R as e,a as i}from"./Row-06bbdda9.js";const n=a=>r("div",{className:"footer",children:r(s,{children:r(e,{children:r(i,{lg:"12",children:r("p",{children:"Copyright © 2023 The Original Knit."})})})})}),c=({children:a})=>o("div",{className:"wrapper",children:[a,r(n,{})]});export{c as W};
